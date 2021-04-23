4dc788588b233a59fe03305684958eb011c4411d
Vadim Rusu <vadim.l.rusu@gmail.com>
2021-04-23 08:10:18 +0100

    update

---
+++ tests/testambr.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import time
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+import smbus
+from smbus import SMBus
+
+from ads112c04 import ads112c04
+
+I2C_sleep_time = 0.2 # seconds to sleep between each channel reading
+bus0 = SMBus(0)
+bus1 = SMBus(1)
+
+addresses = [0x40,0x41,0x43,0x44,0x45,0x46,0x47,0x49,0x4a,0x4c,0x4d,0x4f]
+adcs=[]
+
+
+for i in range(len(addresses)):
+    adc0 = ads112c04.ads112c04(bus0,addresses[i])
+#write register 0, AINP=AIN1, AINN=AVSS, PGA disabled and gain to 1
+    adc0._setmux(0x9)
+#write register 2, set IDAC current to 100uA
+    adc0._setIDAC(0x3)
+#write register 3, route IDAC2 to AIN1
+    adc0._setIDAC2mux(0x2)
+    adcs.append(adc0)
+
+    adc0 = ads112c04.ads112c04(bus1,addresses[i])
+#write register 0, AINP=AIN1, AINN=AVSS, PGA disabled and gain to 1
+    adc0._setmux(0x9)
+#write register 2, set IDAC current to 100uA
+    adc0._setIDAC(0x3)
+#write register 3, route IDAC2 to AIN1
+    adc0._setIDAC2mux(0x2)
+    adcs.append(adc0)
+    
+               
+
+
+for i in range(len(adcs)):
+#send a start/SYNC
+
+    adcs[i]._startconversion()
+
+    time.sleep(I2C_sleep_time)
+
+    data=adcs[i]._readdata()
+
+    print (i,data)
+
+
